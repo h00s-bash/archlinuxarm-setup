9e5f13dd6657e3bea94d4d49ffadbb07b4732c9c
Krunoslav Husak <kruno@binel.hr>
2019-11-09 10:03:28 +0100

    Ability to generate UTF8 locale

--- archarmlinux-setup.sh
+++ archarmlinux-setup.sh
@@ -18,3 +18,20 @@ if [[ $input == "Y" || $input == "y" ]]; then
   echo $new_hostname > /etc/hostname
   echo "New hostname set."
 fi
+
+echo -n "Do you want to set default locale to en_US.UTF-8? [Y,n]? "
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+  sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+  locale-gen
+  echo "LANG=en_US.UTF-8" > /etc/locale.conf
+fi
+
+echo -n "Do you want to update system? [Y,n]? "
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+  pacman-key --init
+  pacman-key --populate archlinuxarm
+  pacman -Syyuw
+  pacman -Syu
+fi
